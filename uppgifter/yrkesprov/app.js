e9239486abe306b5d7eb814867c09c9ba19facaf
Oliver <reeidcsgo@gmail.com>
2021-08-09 08:57:38 +0300

    jhgfds

--- uppgifter/yrkesprov/app.js
+++ uppgifter/yrkesprov/app.js
@@ -1,14 +1,14 @@
-let addBtn = document.querySelector('#add');
-let subBtn = document.querySelector('#sub');
-let qty = document.querySelector('#qtyBox');
+let addBtn = document.getElementById('add');
+let subBtn = document.getElementById('sub');
+let qty = document.getElementById('qtyBox');
 
 addBtn.addEventListener('click',()=>{
     qty.value = parseInt(qty.value) + 1;
 });
 
 subBtn.addEventListener('click',()=>{
-    if (qty.value <= 0) {
-        qty.value = 0;
+    if (qty.value <= -5) {
+        qty.value = -5;
     }
     else{
         qty.value = parseInt(qty.value) - 1;
